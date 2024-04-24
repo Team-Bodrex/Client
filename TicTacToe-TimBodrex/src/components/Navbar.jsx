d30d63828ae747835a61247d1ada0c4a827bf37c
ibnurohman <ibnurohman13@gmail.com>
2024-04-24 19:44:27 +0700

    Buat Navbar

---
+++ TicTacToe-TimBodrex/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    localStorage.removeItem("username");
+    navigate("/login");
+  };
+  return (
+    <div className="bg-gradient-to-r from-gray-700 to-blue-400 p-4 flex flex-col sm:flex-row justify-between items-center">
+      <Link to="/" className="text-white font-bold text-xl mb-4 sm:mb-0">
+        TIC TAC TOE X/O
+      </Link>
+      <div className="flex space-x-4">
+        <button
+          onClick={handleLogout}
+          className="text-white bg-gray-700 px-4 py-2 rounded"
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+}
