45fdddf68b836df809357957302ace4f9016dc95
robbysaputra <robbysaputra300@gmail.com>
2024-04-24 22:00:40 +0700

    protect login done

--- TicTacToe-TimBodrex/src/App.jsx
+++ TicTacToe-TimBodrex/src/App.jsx
@@ -1,56 +1,10 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  redirect,
-} from "react-router-dom";
-import Login from "./pages/login";
-import Game from "./pages/game";
-import Home from "./pages/home";
-import MainLayout from "./pages/MainLayout";
-import Register from "./pages/register";
+import { RouterProvider } from "react-router-dom";
+
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      element: <MainLayout />,
-      loader: () => {
-        if (!localStorage.access_token) {
-          return redirect("/login");
-        }
-        return null;
-      },
-      children: [
-            {
-              path: "/",
-              element: <Home />,
-            },
-            // {
-            //   path: "/home",
-            //   element: <Home />,
-            // },
-            {
-              path: "/game",
-              element: <Game />,
-            },
-          ]
-      },
-      {
-        path: "/register",
-        element: <Register />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-        loader: () => {
-          if (localStorage.access_token) {
-            return redirect("/");
-          }
-          return null;
-        }
-      }
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <>
+    <RouterProvider/>
+    </>
+  )
 }
-
-export default App;
